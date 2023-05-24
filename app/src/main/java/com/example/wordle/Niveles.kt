6b1007058c9a171f3d43230a276a5896d5f7d0d2
Rodrigo <roroeduardo1810@gmail.com>
2023-05-23 22:27:49 -0600

    Primero

---
+++ app/src/main/java/com/example/wordle/Niveles.kt
@@ -0,0 +1,11 @@
+package com.example.wordle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Niveles : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_niveles)
+    }
+}
